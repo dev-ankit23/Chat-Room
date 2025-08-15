eaea5a05e833235dc4f282b8a4b7ef92c1fb01fc
devankitx <126304960+devankitx@users.noreply.github.com>
2025-08-15 11:02:48 +0530

    broadcast messaging

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
+let userCount = 0;
+let allsocket: WebSocket[] = [];
+
 wss.on("connection", (socket) => {
-  socket.on("message", () => {});
+  allsocket.push(socket);
+  userCount = userCount + 1;
+  console.log("User Connected " + userCount);
+
+  socket.on("message", (message) => {
+    console.log("Message Received " + message.toString());
+    allsocket.forEach((s) => {
+      if (s) {
+        // Type guard to ensure socket exists
+        s.send(message.toString() + " :Sent From Server");
+      }
+    });
+  });
 });
